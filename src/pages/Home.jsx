de3e66f07bc84cb032adab760ae3fb24c5bcca0f
JohanManrique10 <edwardjohanm@gmail.com>
2023-11-30 10:17:22 -0500

    fixing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -786,7 +786,16 @@ const Home = () => {
 <form className="max-w-sm mx-auto">
   <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tu opinión</label>
   <textarea id="message" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ejemplo:                                                                               JBL Clip 4                                                                                               Gracias por las especificaciones solucionaron mis dudas..."></textarea>
+
+  <div className="section-2" style={{ margin: "30px 0" }}></div>
+  <a
+                  href="https://www.jbl.com.co/parlantes/XTREME-3-.html"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                >
+                  Enviar
+                </a>
 </form>
+
 
 
 <div className="section-1"></div>
